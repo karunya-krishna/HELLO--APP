79e5b99641917683f303c2048a575fe478fa108e
karunya-krishna <kg8113@srmist.edu.in>
2026-03-11 15:16:15 +0530

    print hello world using multi line commands

--- Hello.java
+++ Hello.java
@@ -1,8 +1,12 @@
-class HelloMessage {
+class HelloMultiple {
     public static void main(String args[]) {
 
         if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+            System.out.print("Hello ");
+            
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i] + " ");
+            }
         } 
         else {
             System.out.println("Hello World");
