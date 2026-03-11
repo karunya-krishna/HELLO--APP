4f92945531d22ed2830827f9fce4bfb2edd303f2
karunya-krishna <kg8113@srmist.edu.in>
2026-03-11 15:09:56 +0530

    printing default messages

--- Hello.java
+++ Hello.java
@@ -1,9 +1,12 @@
-public class Hello {
-    public static void main(String[] args) {
+class HelloMessage {
+    public static void main(String args[]) {
+
         if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
-        } else {
-            System.out.println("Hello!");
+            System.out.println("Hello " + args[0]);
+        } 
+        else {
+            System.out.println("Hello World");
         }
+
     }
 }
